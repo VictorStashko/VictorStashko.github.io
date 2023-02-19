a2b06e04e7a5aeceb558297e4dc400a135031639
Viktor Stashko <victorstashko99@gmail.com>
2023-02-19 20:24:26 +0200

    changed script

--- locator_page/script.js
+++ locator_page/script.js
@@ -1,5 +1,5 @@
 let btnPrimary = document.getElementsByClassName('btn-primary')[0];
 
 btnPrimary.addEventListener('click',()=>{
-    console.log("You clicked on the primary button")
+    btnPrimary.style.backgroundColor = '#FFA500';
 });

28931da7d3240bc0b4e6320ed381f71b39fc8905
Viktor Stashko <victorstashko99@gmail.com>
2023-03-30 17:15:06 +0300

    added js code

--- locator_page/script.js
+++ locator_page/script.js
@@ -3,3 +3,5 @@ let btnPrimary = document.getElementsByClassName('btn-primary')[0];
 btnPrimary.addEventListener('click',()=>{
     btnPrimary.style.backgroundColor = '#FFA500';
 });
+
+obj = page.evaluate("({foo: 'bar'})")
